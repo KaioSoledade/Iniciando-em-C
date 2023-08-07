9e8d53c33d2b5e67bd8aba4afc105307ef449550
Kaio Soledade <soledadekaio@gmail.com>
2023-08-07 09:47:09 -0300

    3 Tentativas

--- Jogo-em-C.c
+++ Jogo-em-C.c
@@ -6,22 +6,28 @@ int main() {
     printf("******************************************\n");
 
     int numerosecreto = 42;
-
     int chute;
 
-    printf("Tente adivinhar o numero");
-    scanf("%d", &chute);
-    printf("seu chute foi %d\r", chute);
-    
-    if(chute == numerosecreto) {
-        printf("Parabens, Voce acertou\r");
-    }
-    else{
-        if (chute > numerosecreto)
-        {
-            printf("Seu chute foi MAIOR\r");
-        } else{
-            printf("Seu chute foi MENOR\r");
+for (int i = 1; i <= 3; i++) {
+        printf("-----------------------------------\n");
+        printf("Tentativa %d de 3\n", i);
+        printf("Qual o seu chute?\n");
+
+        scanf("%d", &chute);
+        
+        int acertou = (chute == numerosecreto);
+
+        if(acertou) {
+            printf("Parabens, Voce acertou\n");
         }
+        else{
+            int maior = (chute > numerosecreto);
+            if (maior)
+            {
+                printf("Seu chute foi MAIOR\n");
+            } else{
+                printf("Seu chute foi MENOR\n");
+            }
+        };
     };
 };
