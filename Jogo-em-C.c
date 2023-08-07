e1cebf913ac7daeca23e82b734c60a3d282614ae
Kaio Soledade <soledadekaio@gmail.com>
2023-08-07 08:52:48 -0300

    FINALIZADO

--- Jogo-em-C.c
+++ Jogo-em-C.c
@@ -7,30 +7,21 @@ int main() {
 
     int numerosecreto = 42;
 
-    printf("o numero %d e numero secreto\n", numerosecreto);
-
     int chute;
+
     printf("Tente adivinhar o numero");
     scanf("%d", &chute);
     printf("seu chute foi %d\r", chute);
-
+    
     if(chute == numerosecreto) {
         printf("Parabens, Voce acertou\r");
     }
     else{
         if (chute > numerosecreto)
         {
-            printf("Voce errou\r");
-            printf("O numero secreto é MENOR\r");
-            printf("Tente Novamente\r");
+            printf("Seu chute foi MAIOR\r");
+        } else{
+            printf("Seu chute foi MENOR\r");
         }
-            if (chute < numerosecreto)
-        {
-            printf("Voce errou\r");
-            printf("O numero secreto e MAIOR\r");
-            printf("Tente Novamente\r");
-        }
-        
-
     };
 };
