51fec85f50bf0153b52839e3fe289bf6cc11d9e5
Kaio Soledade <soledadekaio@gmail.com>
2023-08-08 11:55:14 -0300

    Else if add

--- Jogo-em-C.c
+++ Jogo-em-C.c
@@ -16,19 +16,18 @@ for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
         scanf("%d", &chute);
         
         int acertou = (chute == numerosecreto);
+        int maior = (chute > numerosecreto);
 
         if(acertou) {
             printf("Parabens, Voce acertou\n");
             break;
         }
-        else{
-            int maior = (chute > numerosecreto);
-            if (maior)
-            {
-                printf("Seu chute foi MAIOR, Tente Novamente\n");
-            } else{
-                printf("Seu chute foi MENOR, Tente Novamente\n");
-            }
-        };
+        else if (maior){
+            printf("Seu chute foi MAIOR, Tente Novamente\n");
+
+        } else{
+            printf("Seu chute foi MENOR, Tente Novamente\n");
+            
+        }
     };
 };
