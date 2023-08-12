5453541bdb5dc56922536c73b0e354c85377b035
Kaio Soledade <soledadekaio@gmail.com>
2023-08-12 10:35:39 -0300

    FINALIZADO

--- Jogo-5-Tentativas/Jogo-em-C.c
+++ Jogo-5-Tentativas/Jogo-em-C.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #define NUMERO_DE_TENTATIVAS 5
+
 int main() {
     printf("******************************************\n");
     printf("* Bem vindo ao nosso jogo de adivinhacao *\n");
     printf("******************************************\n");
 
-    int numerosecreto = 42;
+    int segundo = time(0);
+    srand(segundo);
+
+    int pontos = 1000;
+
+    int numerobruto = rand();
+    int numerosecreto = numerobruto % 100;
+
     int chute;
+    printf("%d\n", numerosecreto);
 
 for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
         printf("-----------------------------------\n");
@@ -36,7 +48,11 @@ for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
             printf("Seu chute foi MENOR, Tente Novamente\n");
 
         }
+
+        int pontosperdidos = abs(chute - numerosecreto) / 2;
+        pontos = pontos - pontosperdidos;
     };
-    printf("FIM DE JOGO");
+    printf("FIM DE JOGO\n");
+    printf("Sua Pontuacao: %d\n", pontos);
 
 };
